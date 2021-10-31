4621f87e276e160e055823de5efe7bf1d0e33107
kls-2021 <90022844+kls-2021@users.noreply.github.com>
2021-10-31 23:59:57 +0530

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,16 @@
+var x;
+console.log(x);
+if(x==undefined)
+{
+    console.log("x is undefined"); 
+}
+x=10;
+console.log(x);
+if(x=undefined)
+{
+    console.log("x is undefined"); 
+}
+else
+{
+    console.log("x is defined"); 
+}
